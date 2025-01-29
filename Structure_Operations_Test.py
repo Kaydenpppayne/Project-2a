23784bd92274c897d8b501be58a1efb3e5971650
Kaydenpppayne <kp113410@byu.edu>
2025-01-29 16:57:05 -0700

    Update Structure_Operations_Test.py

--- Structure_Operations_Test.py
+++ Structure_Operations_Test.py
@@ -1,97 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jul 14 14:34:19 2021
+Created on Wed Jul 14 08:51:30 2021
 
 @author: kendrick shepherd
 """
 
 import sys
 
-# determine if the bar is statically determinate (and belongs to a truss)
-def StaticallyDeterminate(nodes,bars):                 
-    # Determine the number of nodes in the truss
-    n_nodes = len(nodes)
-    n_bars = len(bars)
+from ImportCSVData import LoadData
+from Method_of_Joints import IterateUsingMethodOfJoints
+from Structure_Operations import StaticallyDeterminate
+from Structure_Operations import ComputeReactions
+
+import Plotting_Method_of_Joints
+
+# perform the method of joints on a statically
+# determinate truss
+def MethodOfJoints( input_geometry):
     
-    # Determine number of (valid) reactions supported by nodes of the truss
-    n_reactions = 0
+    # load the input data
+    [nodes, bars] = LoadData(input_geometry)
+    
+    # determine if the truss is statically determinate barring parallel or
+    # concurrent reactions
+    if not StaticallyDeterminate(nodes,bars):
+        sys.exit("Cannot operate on a truss that is not statically determinate")
+    
+    # Compute reaction forces at the supports from external loads
+    ComputeReactions(nodes)
+    
+    # Check that correct reaction forces computed
     for node in nodes:
-        if(any(node.ConstraintType())):
-            if(2 in node.ConstraintType()):
-                sys.exit("Truss cannot support a moment reaction force")
-            elif(-1 in node.ConstraintType()):
-                sys.exit("Invalid constraint type specified for the truss")
-            else:
-                n_reactions += len(node.ConstraintType())
+        # supports load in the x direction
+        if 0 in node.ConstraintType():
+            print("Reaction in x at node %d is %f." % (node.idx, node.xforce_reaction))
+        # supports load in the x direction
+        if 1 in node.ConstraintType():
+            print("Reaction in y at node %d is %f." % (node.idx, node.yforce_reaction))
     
-    # Compute if b + r = 2j (Equation 3-1 of the textbook)
-    if(n_bars + n_reactions < 2*n_nodes):
-        sys.exit("The truss is unstable")
-    elif(n_bars + n_reactions > 2*n_nodes):
-        sys.exit("The truss is statically indeterminate, and cannot be resolved using method of joints")
-    else:
-        return True
- 
-def ComputeReactions(nodes):
-    # assume that there is one pin and one roller for our statically determinate structure
-    n_pins = 0
-    n_roller = 0
-    for node in nodes:
-        if(node.constraint=="pin"):
-            pin_node = node
-            n_pins += 1
-        elif(node.constraint=="roller_no_xdisp"):
-            roller_node = node
-            n_roller += 1
-        elif(node.constraint=="roller_no_ydisp"):
-            roller_node = node
-            n_roller += 1
+    # Iterate through all bars using the method of joints
+    # to compute internal member loads
+    IterateUsingMethodOfJoints(nodes,bars)
     
-    if(n_pins != 1 or n_roller != 1):
-        sys.exit("A more clever way must be found to compute the reaction forces")
-    
-    # Continue from here
-    # Sum of moments about the pin
-    [pin_x, pin_y] = pin_node.location
-    [roller_x, roller_y] = roller_node.location
-    
-    roller_reaction = 0
-    for node in nodes:
-        [node_x,node_y] = node.location
-        # contributions in the y direction
-        roller_reaction += node.yforce_external * (node_x - pin_x)
-        # contributions in the x direction
-        roller_reaction += node.xforce_external * (pin_y - node_y)
-        
-    if(roller_node.constraint=="roller_no_xdisp"):
-            roller_reaction = -roller_reaction/(pin_y - roller_y)
-            roller_node.AddReactionXForce(roller_reaction)
-    elif(roller_node.constraint=="roller_no_ydisp"):
-            roller_reaction = -roller_reaction/(roller_x - pin_x)
-            roller_node.AddReactionYForce(roller_reaction)
-            
-   
-    # sum of forces in y direction
-    
-    for node in nodes:
-        pin_y+=node.yforce_external/2
-        
-        if(node.xforce_external == 0):
-            roller_reaction += -roller_reaction - pin_y
-            pin_x = pin_x
-            pin_y = -roller_reaction
-            
-        if(node.xforce_external != 0):
-            pin_x += node.xforce_external
-            pin_y = -roller_reaction
-            roller_reaction += -roller_reaction - pin_y
-            
-        pin_node.AddReactionYForce(-pin_y)
-       
-        pin_node.AddReactionXForce(-pin_x)
-       
-        
-    # sum of forces in x direction
-    
-    
+    # output computed forces in bars
+    for bar in bars:
+        val = round(bar.axial_load,2)
+        print(bar.idx,val)
+
+    # plotting functionality to visualize indices
+    # see the problem statement for how to plot alternative information
+    Plotting_Method_of_Joints.PlotStructureData(nodes,bars,"index")
+
+# Run the plane truss function 
+MethodOfJoints('Example_3_2.csv')
