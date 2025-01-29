6146b9bb1c949a9d4dab7d3697ffacfdcff842ad
Kaydenpppayne <kp113410@byu.edu>
2025-01-29 16:53:43 -0700

    Update Structure_Operations_Test.py

--- Structure_Operations_Test.py
+++ Structure_Operations_Test.py
@@ -1,42 +1,97 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 20 08:20:47 2025
+Created on Wed Jul 14 14:34:19 2021
 
-@author: kendrickshepherd
+@author: kendrick shepherd
 """
 
-import Main_for_Testing
+import sys
 
-import unittest
-
-class TestStructureOperations(unittest.TestCase):
-
-    def test_Example_3_2(self):
-        nodes,bars = Main_for_Testing.MethodOfJoints("Example_3_2.csv")
-        # print(nodes[0].constraint)
-        # print(nodes[3].constraint)
-        # print(nodes[0].xforce_reaction)
-        # print(nodes[0].yforce_reaction)
-        # print(nodes[3].yforce_reaction)
-        decimal_place = 3
+# determine if the bar is statically determinate (and belongs to a truss)
+def StaticallyDeterminate(nodes,bars):                 
+    # Determine the number of nodes in the truss
+    n_nodes = len(nodes)
+    n_bars = len(bars)
+    
+    # Determine number of (valid) reactions supported by nodes of the truss
+    n_reactions = 0
+    for node in nodes:
+        if(any(node.ConstraintType())):
+            if(2 in node.ConstraintType()):
+                sys.exit("Truss cannot support a moment reaction force")
+            elif(-1 in node.ConstraintType()):
+                sys.exit("Invalid constraint type specified for the truss")
+            else:
+                n_reactions += len(node.ConstraintType())
+    
+    # Compute if b + r = 2j (Equation 3-1 of the textbook)
+    if(n_bars + n_reactions < 2*n_nodes):
+        sys.exit("The truss is unstable")
+    elif(n_bars + n_reactions > 2*n_nodes):
+        sys.exit("The truss is statically indeterminate, and cannot be resolved using method of joints")
+    else:
+        return True
+ 
+def ComputeReactions(nodes):
+    # assume that there is one pin and one roller for our statically determinate structure
+    n_pins = 0
+    n_roller = 0
+    for node in nodes:
+        if(node.constraint=="pin"):
+            pin_node = node
+            n_pins += 1
+        elif(node.constraint=="roller_no_xdisp"):
+            roller_node = node
+            n_roller += 1
+        elif(node.constraint=="roller_no_ydisp"):
+            roller_node = node
+            n_roller += 1
+    
+    if(n_pins != 1 or n_roller != 1):
+        sys.exit("A more clever way must be found to compute the reaction forces")
+    
+    # Continue from here
+    # Sum of moments about the pin
+    [pin_x, pin_y] = pin_node.location
+    [roller_x, roller_y] = roller_node.location
+    
+    roller_reaction = 0
+    for node in nodes:
+        [node_x,node_y] = node.location
+        # contributions in the y direction
+        roller_reaction += node.yforce_external * (node_x - pin_x)
+        # contributions in the x direction
+        roller_reaction += node.xforce_external * (pin_y - node_y)
         
-        self.assertAlmostEqual(0, nodes[0].xforce_reaction, decimal_place)
-        self.assertAlmostEqual(4, nodes[3].yforce_reaction, decimal_place)
-        self.assertAlmostEqual(4, nodes[3].yforce_reaction, decimal_place)
-
-    def test_Example_3_3(self):
-        nodes,bars = Main_for_Testing.MethodOfJoints("Example_3_3.csv")
-        # print(nodes[0].constraint)
-        # print(nodes[4].constraint)
-        # print(nodes[0].xforce_reaction)
-        # print(nodes[0].yforce_reaction)
-        # print(nodes[4].yforce_reaction)
-        decimal_place = 3
+    if(roller_node.constraint=="roller_no_xdisp"):
+            roller_reaction = -roller_reaction/(pin_y - roller_y)
+            roller_node.AddReactionXForce(roller_reaction)
+    elif(roller_node.constraint=="roller_no_ydisp"):
+            roller_reaction = -roller_reaction/(roller_x - pin_x)
+            roller_node.AddReactionYForce(roller_reaction)
+            
+   
+    # sum of forces in y direction
+    
+    for node in nodes:
+        pin_y+=node.yforce_external/2
         
-        self.assertAlmostEqual(-141.42136, nodes[0].xforce_reaction, decimal_place)
-        self.assertAlmostEqual(125.39385, nodes[0].yforce_reaction, decimal_place)
-        self.assertAlmostEqual(191.0275, nodes[4].yforce_reaction, decimal_place)
-
-if __name__ == '__main__':
-    unittest.main()
+        if(node.xforce_external == 0):
+            roller_reaction += -roller_reaction - pin_y
+            pin_x = pin_x
+            pin_y = -roller_reaction
+            
+        if(node.xforce_external != 0):
+            pin_x += node.xforce_external
+            pin_y = -roller_reaction
+            roller_reaction += -roller_reaction - pin_y
+            
+        pin_node.AddReactionYForce(-pin_y)
+       
+        pin_node.AddReactionXForce(-pin_x)
+       
+        
+    # sum of forces in x direction
+    
+    
