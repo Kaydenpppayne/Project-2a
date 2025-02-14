08af83078cc60cdfec5898b0b6b3944d9474931d
Kaydenpppayne <kp113410@byu.edu>
2025-02-13 20:13:37 -0700

    Add files via upload

---
+++ Geometry_Operations_Pre_class.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 11:25:01 2021
+
+@author: kendrick shepherd
+"""
+
+import math
+import numpy as np
+import sys
+
+# length of the beam
+def Length(bar):
+    # find a node of the bar
+    bar_node = bar.int_node
+    # convert the node and bar to a vector
+    vector = BarNodeToVector(bar_node, bar)
+    # find the length of the vector
+    length = VectorTwoNorm(vector)
+    # output the vector length
+    return length
+
+# Find two norm (magnitude) of a vector
+def VectorTwoNorm(vector):
+    norm = 0
+    for i in range(0,len(vector)):
+        norm += vector[i]**2
+    return np.sqrt(norm)
+
+# Find a shared node between two bars
+def FindSharedNode(bar_1,bar_2):
+    if(bar_1.init_node==bar_2.init_node):
+        return bar_1.init_node
+    elif(bar_1.init_node==bar_2.end_node):
+        return bar_1.init_node
+    elif(bar_1.end_node==bar_2.end_node):
+        return bar_1.end_node
+    elif(bar_1.end_node==bar_2.init_node):
+        return bar_1.end_node
+    else:
+        sys.exit("The two input bars do not share a node")
+
+# Given a bar and a node on that bar, find the other node
+def FindOtherNode(node,bar):
+    if(bar.init_node==node):
+        return bar.end_node
+    elif(bar.end_node == node):
+        return bar.init_node
+    else:
+        sys.exit("The input node is not on the bar")
+    
+
+# Find a vector from input node (of the input bar) in the direction of the bar
+def BarNodeToVector(origin_node,bar):
+    other_node = FindOtherNode(origin_node, bar)
+    origin_loc = origin_node.location
+    other_loc = other_node.location
+    vec = [other_loc[0]-origin_loc[0], other_loc[1]-origin_loc[1]]
+    return vec
+
+# Convert to bars that meet at a node into vectors pointing away from that node
+def BarsToVectors(bar_1,bar_2):
+    FindSharedNode(bar_1,bar_2)
+    bar_1_vec = BarNodeToVector(bar_1.init_node,bar_1)
+    bar_2_vec = BarNodeToVector(bar_2.init_node,bar_2)
+    return bar_1_vec, bar_2_vec 
+
+# Cross product of two vectors
+def TwoDCrossProduct(vec1,vec2):
+    crossproduct = vec1[0]*vec2[1]-vec1[1]*vec2[0]
+    return crossproduct
+
+# Dot product of two vectors
+def DotProduct(vec1,vec2):
+    dotproduct=0
+    for i in range(0,len(vec1)):
+        dotproduct+=vec1[i]*vec2[i]
+    #dotproduct = vec1[0]*vec2[0]+vec1[1]*vec2[1]
+    return dotproduct
+
+# Cosine of angle from local x vector direction to other vector
+def CosineVectors(local_x_vec,other_vec):
+    vecdot = DotProduct(local_x_vec, other_vec)
+    normnorm = VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec)
+    cosang = vecdot/normnorm
+    return cosang
+
+# Sine of angle from local x vector direction to other vector
+def SineVectors(local_x_vec,other_vec):
+    veccross = TwoDCrossProduct(local_x_vec, other_vec)
+    normnorm = VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec)
+    sinang = veccross/normnorm
+    return sinang
+
+# Cosine of angle from local x bar to the other bar
+def CosineBars(local_x_bar,other_bar):
+    vec1, vec2 = BarsToVectors(local_x_bar, other_bar)
+    cosbar = CosineVectors(vec1, vec2)
+    return cosbar
+
+# Sine of angle from local x bar to the other bar
+def SineBars(local_x_bar,other_bar):
+    vec1, vec2 = BarsToVectors(local_x_bar, other_bar)
+    sinbar = SineVectors(vec1, vec2)
+    return sinbar
--- Structure_Operations.py
+++ Structure_Operations.py
@@ -53,9 +53,70 @@ def ComputeReactions(nodes):
     
     # Continue from here
     # Sum of moments about the pin
-
+    [pin_x, pin_y] = pin_node.location
+    [roller_x, roller_y] = roller_node.location
+    
+    roller_reaction = 0
+    for node in nodes:
+        [node_x,node_y] = node.location
+        # contributions in the y direction
+        roller_reaction += node.yforce_external * (node_x - pin_x)
+        # contributions in the x direction
+        roller_reaction += node.xforce_external * (pin_y - node_y)
+        
+    if(roller_node.constraint=="roller_no_xdisp"):
+            roller_reaction = -roller_reaction/(pin_y - roller_y)
+            roller_node.AddReactionXForce(roller_reaction)
+    elif(roller_node.constraint=="roller_no_ydisp"):
+            roller_reaction = -roller_reaction/(roller_x - pin_x)
+         #   roller_reaction = -roller_reaction
+            roller_node.AddReactionYForce(roller_reaction)
+            
+   
     # sum of forces in y direction
-
+    pin_reactionx = 0 
+    pin_reactiony = 0
+    for node in nodes:
+    #    [node_x,node_y] = node.location
+        
+        pin_reactiony += node.yforce_external
+        
+    for node in nodes:
+        pin_reactionx += node.xforce_external
+        
+    if(roller_node.constraint=="roller_no_xdisp"):
+        pin_reactionx = pin_reactionx + roller_reaction
+        
+        
+    elif(roller_node.constraint=="roller_no_ydisp"):
+        pin_reactiony=pin_reactiony + roller_reaction
+        
+    pin_reactiony=-pin_reactiony
+    pin_reactionx=-pin_reactionx
+    pin_node.AddReactionXForce(pin_reactionx)
+    pin_node.AddReactionYForce(pin_reactiony)
+ #   elif(pin_node.constraint=="pin"):
+    #    pin_reactionx=pin_reaction-roller_reaction
+    #    pin_node.AddReactionXForce(pin_reactionx)
+        
+  #      pin_reactiony=pin_reaction+pin_y+roller_y
+  #      pin_node.AddReactionYForce(pin_reactiony)
+        
+     #   if(node.xforce_external == 0):
+    #        roller_reaction += -roller_reaction - pin_y
+       #     pin_x = pin_x
+        #    pin_y = -roller_reaction
+            
+   #     if(node.xforce_external != 0):
+       #     pin_x += node.xforce_external
+      #      pin_y = -roller_reaction
+        #    roller_reaction += -roller_reaction - pin_y
+            
+    #    pin_node.AddReactionYForce(-pin_y)
+       
+     #   pin_node.AddReactionXForce(-pin_x)
+       
+        
     # sum of forces in x direction
     
     
